f8013ecbe598d574346cae2be194bf570a81f5c8
Jared Geilich <jgeilich@gmail.com>
2018-02-10 17:04:04 -0800

    improved mobile ui

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -9,11 +9,15 @@ class Connect extends React.Component {
                 <div className='stars2' />
                 <div className='stars3' />
                 <h2>Connect With Me</h2>
-                <div className="icons">
-                    <a href='https://www.linkedin.com/in/jgeilich/'><i className="fab fa-linkedin fa-3x" /></a>
-                    <a href='https://github.com/jaredgei'><i className="fab fa-github-square fa-3x" /></a>
-                    <a href='https://www.facebook.com/jaredgei'><i className="fab fa-facebook fa-3x" /></a>
-                    <a href='mailto:jgeilich@gmail.com'><i className="fas fa-envelope-square fa-3x" /></a>
+                <div className='icons'>
+                    <div className='iconRow'>
+                        <a href='https://www.linkedin.com/in/jgeilich/'><i className='fab fa-linkedin fa-3x' /></a>
+                        <a href='https://github.com/jaredgei'><i className='fab fa-github-square fa-3x' /></a>
+                    </div>
+                    <div className='iconRow'>
+                        <a href='https://www.facebook.com/jaredgei'><i className='fab fa-facebook fa-3x' /></a>
+                        <a href='mailto:jgeilich@gmail.com'><i className='fas fa-envelope-square fa-3x' /></a>
+                    </div>
                 </div>
             </div>
         );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,14 @@ class Projects extends React.Component {
     render() {
         return (
             <div className='projects'>
-                <a className='card' href='https://soundcloud.com/swordfishdoula'><img src='images/sfd.png' /></a>
-                <a className='card' href='http://potlux.org/'><img src='images/potlux.png' /></a>
-                <a className='card' href='https://grokker.com/'><img src='images/grokker.png' /></a>
-                <a className='card' href='http://thestallwall.com/'><img src='images/stallwall.png' /></a>
+                <div className='cardRow'>
+                    <a className='card' href='https://soundcloud.com/swordfishdoula'><img src='images/sfd.png' /></a>
+                    <a className='card' href='http://potlux.org/'><img src='images/potlux.png' /></a>
+                </div>
+                <div className='cardRow'>
+                    <a className='card' href='https://grokker.com/'><img src='images/grokker.png' /></a>
+                    <a className='card' href='http://thestallwall.com/'><img src='images/stallwall.png' /></a>
+                </div>
             </div>
         );
     }
